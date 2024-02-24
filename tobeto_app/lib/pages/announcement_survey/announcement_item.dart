d5d5c61852659ddf950d84e3b4ff549489f2645f
Mehtap Aygün <mehtap.aygun@icloud.com>
2024-02-24 13:11:56 +0300

    alert announcement add

--- tobeto_app/lib/pages/announcement_survey/announcement_item.dart
+++ tobeto_app/lib/pages/announcement_survey/announcement_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tobeto_app/config/constant/core/widget/neu_box.dart';
 import 'package:tobeto_app/config/constant/theme/text_theme.dart';
@@ -13,47 +14,78 @@ class AnnouncementItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
-      child: NeuBox(
-        width: 250,
-        child: Column(
-          children: [
-            const Spacer(
-              flex: 3,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: AppTextTheme.small(announcement.title, context,
-                  textAlign: TextAlign.center),
-            ),
-            const Spacer(
-              flex: 3,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      FontAwesomeIcons.calendar,
-                      color: Colors.blue.shade900,
-                      size: 18,
-                    ),
-                    const SizedBox(width: 8),
-                    AppTextTheme.xxSmall("${announcement.dateTime}", context)
-                  ],
+      child: InkWell(
+        child: NeuBox(
+          width: 250,
+          child: Column(
+            children: [
+              const Spacer(
+                flex: 3,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(50),
                 ),
-                Icon(
-                  Icons.touch_app_rounded,
-                  color: Colors.blue.shade900,
-                  size: 22,
-                )
-              ],
-            ),
-            const Spacer(
-              flex: 1,
-            )
-          ],
+                padding: const EdgeInsets.all(15),
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                child: AppTextTheme.xSmall(announcement.title, context,
+                    textAlign: TextAlign.center),
+              ),
+              const Spacer(
+                flex: 3,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.calendar,
+                        color: Colors.blue.shade900,
+                        size: 18,
+                      ),
+                      const SizedBox(width: 8),
+                      AppTextTheme.xxSmall("${announcement.dateTime}", context)
+                    ],
+                  ),
+                  Icon(
+                    Icons.touch_app_rounded,
+                    color: Colors.blue.shade900,
+                    size: 22,
+                  )
+                ],
+              ),
+              const Spacer(
+                flex: 1,
+              )
+            ],
+          ),
         ),
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Kapat"))
+              ],
+              title: AppTextTheme.small(
+                announcement.title,
+                context,
+                textAlign: TextAlign.center,
+              ),
+              contentPadding: const EdgeInsets.all(15),
+              content: SingleChildScrollView(
+                child: AppTextTheme.xSmall(announcement.text, context,
+                    fontWeight: FontWeight.normal),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
